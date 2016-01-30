2b6601d528d53019b3fc77acd489a9745982b70d
Hadrian Tang <hadrianwttang@outlook.com>
2016-01-30 13:19:04 +0000

    Changeset:7

---
+++ MCRealm/Load From....vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows.Forms
+
+Public Class Load_From
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Dim Result As System.Windows.Forms.DialogResult = Folder.ShowDialog()
+        _Path = Folder.SelectedPath
+        [Exit](Result)
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Dim Result As System.Windows.Forms.DialogResult = File.ShowDialog()
+        _Path = File.FileName
+        [Exit](Result)
+    End Sub
+
+    Private Sub [Exit](ByVal Result As System.Windows.Forms.DialogResult)
+        Me.DialogResult = Result
+        Me.Close()
+    End Sub
+
+    Public Property Path As String
+End Class
