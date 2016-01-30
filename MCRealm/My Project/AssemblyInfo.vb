ecf55a7711e3ddb88469ef068d1726412fcb7a9a
Hadrian Tang <hadrianwttang@outlook.com>
2016-01-30 13:19:46 +0000

    Changeset:8

---
+++ MCRealm/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MCRealm")> 
+<Assembly: AssemblyDescription("Alternative to Minecraft Realms.")> 
+<Assembly: AssemblyCompany("Happypig375")> 
+<Assembly: AssemblyProduct("MCRealm")> 
+<Assembly: AssemblyCopyright("Copyright ©  Happypig375 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a1cb1244-6489-4087-9ab4-c1c19d8b634e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.1.0.0")> 
+<Assembly: AssemblyFileVersion("0.1.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en-US")> 
