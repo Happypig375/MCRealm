6ee5094043111173f467771dd89d111f93521f72
Hadrian Tang <hadrianwttang@outlook.com>
2016-08-18 11:16:30 +0000

    Bugfix for server restart request in settings
    git-tfs-id: [https://happypig-375.visualstudio.com]$/MCRealm/Visual studio projects;C96

---
+++ CMD/AltCMD/AltCMD.vb
@@ -0,0 +1,8 @@
+﻿Module AltCMD
+
+    Sub Main()
+        Console.WriteLine($"Microsoft Windows [Version {My.Computer.Info.OSVersion.ToString}]")
+        Console.ReadKey()
+    End Sub
+
+End Module
